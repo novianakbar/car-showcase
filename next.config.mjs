f76553f6d886a831fe7efdc8aa2a42e98aa852f7
novianakbar <akbarian555@gmail.com>
2024-02-06 13:46:36 +0700

    reconfig next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/car-showcase",
   images: {
     domains: ["cdn.imagin.studio"],
-    unoptimized: true,
   },
 };
 
